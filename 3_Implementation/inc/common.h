4a5e13dc96ee6b5073b61ff52d311a3dc619ecdc
Deeksha Tripathi <deeksha18799@gmail.com>
2021-09-07 11:12:21 +0530

    Completed Include file

---
+++ 3_Implementation/inc/common.h
@@ -0,0 +1,20 @@
+/** 
+* @author Deeksha Tripathi(deeksha18799@gmail.com)
+* @file common.h
+* @brief single declaration of datatypes used by multiple files
+*
+*/
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+typedef enum
+{
+    SUCCESS,
+    FAILURE,
+    NULL_PTR,
+    EMPTY_STRING
+
+} status;
+
+#endif
